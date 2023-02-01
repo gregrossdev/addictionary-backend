b91e8244db0d29b139f1bdc1b6405e5df27287e9
Gregory Ross <gregross.dev@gmail.com>
2023-02-01 14:47:34 -0500

    redirect to listing resource page

--- app/Filament/Resources/TermResource/Pages/EditTerm.php
+++ app/Filament/Resources/TermResource/Pages/EditTerm.php
@@ -10,6 +10,11 @@ class EditTerm extends EditRecord
 {
     protected static string $resource = TermResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getActions(): array
     {
         return [
